a9ca12fb091a26277fa9e59b197ac12c406c2d27
Rafael <rafamarques9@me.com>
2017-11-14 12:11:50 +0100

    Initial commit

---
+++ Marvel Superheroes/Model/SuperheroModel.swift
@@ -0,0 +1,13 @@
+//
+//  SuperheroModel.swift
+//  Marvel Superheroes
+//
+//  Created by Rafa Marques on 13.11.17.
+//  Copyright © 2017 Rafa Marques. All rights reserved.
+//
+
+import Foundation
+
+class SuperheroModel {
+    
+}
